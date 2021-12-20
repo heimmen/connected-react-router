8d0cb39564035f4be547c9112eeb4f1dcec74082
Chong Liu <chongliu@cn.ibm.com>
2021-12-19 19:54:11 -0800

    Initially run devserver in basic sample

--- examples/basic/webpack.config.js
+++ examples/basic/webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path')
-const webpack = require('webpack')
+const webpack = require('webpack');
+const port = 1214;
+
+const publicPath = `http://localhost:${port}/dist`;
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   entry: [
     'react-hot-loader/patch',
-    'webpack-hot-middleware/client',
+    // `webpack-dev-server/client?http://localhost:${port}/`,
     path.resolve('src/index.js'),
   ],
+  context: __dirname,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -32,4 +36,53 @@ module.exports = {
       'node_modules',
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    static: {
+      directory: __dirname
+    },
+	// 	port,
+	// 	host: 'localhost',
+	// 	compress: true,
+	// 	hot: true,
+	// 	allowedHosts: 'all',
+	// 	headers: { 'Access-Control-Allow-Origin': '*' },
+	// 	static: {
+	// 		directory: path.join(__dirname, 'dist'),
+	// 		publicPath
+	// 	},
+	// 	watchFiles: {
+	// 		paths: __dirname,
+	// 		options: {
+	// 			aggregateTimeout: 1000,
+	// 			ignored: [
+	// 				path.resolve(__dirname, 'node_modules'),
+	// 				path.resolve(__dirname, 'app/node_modules'),
+	// 				path.resolve(__dirname, 'stats_backend'),
+	// 				path.resolve(__dirname, 'Samples'),
+	// 				path.resolve(__dirname, 'flow-typed'),
+	// 				path.resolve(__dirname, '.git'),
+	// 				path.resolve(__dirname, 'dll')
+	// 			],
+	// 			poll: 3000
+	// 		}
+	// 	},
+	// 	historyApiFallback: {
+	// 		verbose: true,
+	// 		disableDotRule: false
+	// 	},
+	// 	onBeforeSetupMiddleware: () => {
+	// 		if (process.env.START_HOT) {
+	// 			console.log('Starting Main Process...'); // eslint-disable-line
+	// 			spawn('npm', ['run', 'start-main-dev'], {
+	// 				shell: true,
+	// 				env: process.env,
+	// 				stdio: 'inherit'
+	// 			})
+	// 				.on('close', code => process.exit(code)) // eslint-disable-line
+	// 				.on('error', (spawnError) => console.error(spawnError));
+	// 		}
+	// 	}
+	}
 }
